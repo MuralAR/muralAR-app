a867b456bda952453ce155850fef72af2f8cbd63
CormacAsGaeilge <cwhite788@gmail.com>
2020-11-14 13:53:23 +0000

    Added ARViewer UI component and assets

---
+++ Assets/Scripts/ARUIManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class ARUIManager : MonoBehaviour
+{
+    public Color offColor, onColor;
+    public GameObject videoPlayer;
+    private VideoPlayer mVideoPlayer;
+    private RawImage mRawImage;
+
+    public bool turnOn = false;
+    private bool oldVal = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mVideoPlayer = videoPlayer.GetComponent<VideoPlayer>();
+        mRawImage = videoPlayer.GetComponent<RawImage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(turnOn != oldVal) {
+            oldVal = turnOn;
+            ToggleVideoPlayer(turnOn);
+        }
+    }
+
+    void ToggleVideoPlayer(bool turnOn)
+    {
+        if (turnOn)
+        {
+            mRawImage.color = offColor;
+            mVideoPlayer.Play();
+        }
+        else
+        {
+            mRawImage.color = onColor;
+            mVideoPlayer.Stop();
+        }
+    }
+}
