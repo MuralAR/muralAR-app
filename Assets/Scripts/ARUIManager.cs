7312d8e0255bc517d122744df02dc49140f51fa0
dcappieri <dcappieri@digisoft.tv>
2020-11-20 16:52:08 +0000

    adding icon to map

--- Assets/Scripts/ARUIManager.cs
+++ Assets/Scripts/ARUIManager.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
 public class ARUIManager : MonoBehaviour
 {
@@ -11,21 +13,24 @@ public class ARUIManager : MonoBehaviour
     public GameObject mapImage;
     public GameObject magicIcon;
     public GameObject reasonBox;
+    public GameObject distanceBox;
 
     public string reasonBoxText;
     private TMP_Text reasonBoxTMP;
-
-
+    private TMP_Text locationBoxTMP;
     public GameObject moreInfoButton;
     public GameObject moreInfoBox;
 
     public bool videoPlayerState;
+    private Dictionary<string, string> muralLocations = new Dictionary<string, string>();
     // Start is called before the first frame update
     void Start()
     {
         reasonBoxTMP = reasonBox.GetComponentInChildren<TMP_Text>();
         reasonBoxTMP.text = reasonBoxText;
+        locationBoxTMP = distanceBox.GetComponentInChildren<TMP_Text>();
         ToggleVideoPlayer(true);
+        SetMuralDistance();
     }
 
     // Update is called once per frame
@@ -44,6 +49,7 @@ public class ARUIManager : MonoBehaviour
             moreInfoBox.SetActive(false);
             mapImage.SetActive(false);
             magicIcon.SetActive(false);
+            distanceBox.SetActive(false);
         }
         else
         {
@@ -53,6 +59,7 @@ public class ARUIManager : MonoBehaviour
             moreInfoBox.SetActive(false);
             mapImage.SetActive(false);
             magicIcon.SetActive(false);
+            distanceBox.SetActive(false);
         }
     }
 
@@ -65,6 +72,7 @@ public class ARUIManager : MonoBehaviour
             reasonBox.SetActive(false);
             moreInfoButton.SetActive(false);
             moreInfoBox.SetActive(false);
+            distanceBox.SetActive(false);
         }
     }
 
@@ -77,6 +85,7 @@ public class ARUIManager : MonoBehaviour
             reasonBox.SetActive(false);
             moreInfoButton.SetActive(true);
             moreInfoBox.SetActive(false);
+            distanceBox.SetActive(false);
         }
     }
 
@@ -96,5 +105,24 @@ public class ARUIManager : MonoBehaviour
             moreInfoButton.SetActive(true);
             moreInfoBox.SetActive(false);
         }
+    }
+
+    private void SetMuralDistance()
+    {
+        muralLocations.Add("icon1","Mural 1. You are 206 meters away");
+        muralLocations.Add("icon2","Mural 2. You are 505 meters away");
+        muralLocations.Add("icon3","Mural 3. You are 648 meters away");
+    }
+    
+    private void CalculateDistance(string icon)
+    {
+        string distance =  muralLocations[icon];
+        locationBoxTMP.text = distance;
+    }
+    
+    public void OnClickedIcon(){ 
+        string thisPosition = EventSystem.current.currentSelectedGameObject.name;
+        CalculateDistance(thisPosition);
+        distanceBox.SetActive(true);
     }
 }
