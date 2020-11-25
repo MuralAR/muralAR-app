c42502220128cab6d78cd9a317b0a03b171885df
CormacAsGaeilge <cwhite788@gmail.com>
2020-11-25 19:05:28 +0000

    Created MuralData Scriptable Object

---
+++ Assets/Data/MuralData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuralData : ScriptableObject 
+{
+    public string title = "Mural Title";
+    public string description = "Mural Descriptiom";
+    public string artist = "Mural Artist";
+    public Texture[] images;
+    public LocationInfo location;
+}
