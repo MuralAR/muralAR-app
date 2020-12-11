d76a08055436b8824ef145cbe494727be0c7a431
dcappieri <dcappieri@digisoft.tv>
2020-12-11 14:57:17 +0000

    Map view bug fixes

--- Assets/Scripts/ARUIManager.cs
+++ Assets/Scripts/ARUIManager.cs
@@ -12,6 +12,7 @@ public class ARUIManager : MonoBehaviour
     public GameObject videoPlayer;
     public GameObject mapImage;
     public GameObject magicIcon;
+    public GameObject mapIcon;
     public GameObject reasonBox;
     public GameObject distanceBox;
     public GameObject infoBox;
@@ -30,6 +31,9 @@ public class ARUIManager : MonoBehaviour
     private Dictionary<string, string> muralLocations = new Dictionary<string, string>();
 
     private Dictionary<string, string> muralInfo = new Dictionary<string, string>();
+
+    public Sprite[] muralImages;
+    public GameObject imagePanel;
 
     // Start is called before the first frame update
     void Start()
@@ -62,6 +66,7 @@ public class ARUIManager : MonoBehaviour
             magicIcon.SetActive(false);
             distanceBox.SetActive(false);
             infoBox.SetActive(false);
+            mapIcon.SetActive(true);
         }
         else
         {
@@ -74,37 +79,37 @@ public class ARUIManager : MonoBehaviour
             magicIcon.SetActive(false);
             distanceBox.SetActive(false);
             infoBox.SetActive(false);
+            mapIcon.SetActive(true);
         }
     }
 
     public void OpenMap()
     {
-        {
-            mapImage.SetActive(true);
-            magicIcon.SetActive(true);
-            videoPlayer.SetActive(false);
-            reasonBox.SetActive(false);
-            moreInfoButton.SetActive(false);
-            LeanTween.alpha(moreInfoButton.GetComponent<RectTransform>(), 0f, 0.3f);
-            moreInfoBox.SetActive(false);
-            distanceBox.SetActive(false);
-            infoBox.SetActive(false);
-        }
+        mapImage.SetActive(true);
+        magicIcon.SetActive(true);
+        videoPlayer.SetActive(false);
+        reasonBox.SetActive(false);
+        moreInfoButton.SetActive(false);
+        LeanTween.alpha(moreInfoButton.GetComponent<RectTransform>(), 0f, 0.3f);
+        moreInfoBox.SetActive(false);
+        distanceBox.SetActive(false);
+        infoBox.SetActive(false);
+        mapIcon.SetActive(false);
     }
 
     public void CloseMap()
     {
-        {
-            mapImage.SetActive(false);
-            magicIcon.SetActive(false);
-            videoPlayer.SetActive(false);
-            reasonBox.SetActive(false);
-            moreInfoButton.SetActive(true);
-            LeanTween.alpha(moreInfoButton.GetComponent<RectTransform>(), 1f, 0.6f);
-            moreInfoBox.SetActive(false);
-            distanceBox.SetActive(false);
-            infoBox.SetActive(false);
-        }
+        mapImage.SetActive(false);
+        magicIcon.SetActive(false);
+        videoPlayer.SetActive(false);
+        reasonBox.SetActive(false);
+        moreInfoButton.SetActive(true);
+        LeanTween.alpha(moreInfoButton.GetComponent<RectTransform>(), 1f, 0.6f);
+        moreInfoBox.SetActive(false);
+        distanceBox.SetActive(false);
+        infoBox.SetActive(false);
+        mapIcon.SetActive(true);
+
     }
 
     public void ToggleMoreInfo(bool turnOn)
@@ -186,15 +191,16 @@ public class ARUIManager : MonoBehaviour
     private void OnClickedMoreInfoMap(String icon)
     {
         infoBox.SetActive(true);
-        LeanTween.scale(infoBox, new Vector3(0.1719052f, 0.1409813f, 0.31823f), 0.5f);
+        LeanTween.scale(infoBox, new Vector3(0.1719052f, 0.1409813f, 0.31823f), 0.3f);
         distanceBox.SetActive(false);
         locationMoreInfoBoxTMP.text = muralInfo[icon];
+        SetMuralImage(icon);
     }
 
     public void OnClickedMap()
     {
         distanceBox.SetActive(false);
-        LeanTween.scale(infoBox, new Vector3(0, 0, 0), 0.5f).setOnComplete(DeactivateInfoBox);
+        LeanTween.scale(infoBox, new Vector3(0, 0, 0), 0.3f).setOnComplete(DeactivateInfoBox);
     }
 
     void DeactivateInfoBox()
@@ -204,18 +210,31 @@ public class ARUIManager : MonoBehaviour
 
     private void SetMuralDistance()
     {
-        muralLocations.Add("icon1", "Mural 1. You are 155 meters away");
-        muralLocations.Add("icon2", "Mural 2. You are 245 meters away");
-        muralLocations.Add("icon3", "Mural 3. You are 648 meters away");
+        muralLocations.Add("icon1", "Mural: The Last King of Ireland, \n155 meters away [S]");
+        muralLocations.Add("icon2", "Mural: Brigid of Faughart, \n245 meters away [N]");
+        muralLocations.Add("icon3", "Mural: Ní neart go cur le chéile,\n648 meters away [N]");
     }
 
     private void SetMuralInfo()
     {
-        muralInfo.Add("icon1",
-            "ARTIST - achesdub \nThe Last King of Ireland  \nHailing from Dublin, Aches has been creating work since the age of fifteen. Boasting an impressive repertoire of work, he has been invited to design and create original artwork for projects in countries such as Denmark, Ireland, Hungary, Spain, Sweden, Miami, Scotland, Austria, Switzerland, England and USA.");
-        muralInfo.Add("icon2",
-            "ARTIST - achesdub \nThe Last King of Ireland  \nHailing from Dublin, Aches has been creating work since the age of fifteen. Boasting an impressive repertoire of work, he has been invited to design and create original artwork for projects in countries such as Denmark, Ireland, Hungary, Spain, Sweden, Miami, Scotland, Austria, Switzerland, England and USA.");
-        muralInfo.Add("icon3",
-            "ARTIST - achesdub \nThe Last King of Ireland  \nHailing from Dublin, Aches has been creating work since the age of fifteen. Boasting an impressive repertoire of work, he has been invited to design and create original artwork for projects in countries such as Denmark, Ireland, Hungary, Spain, Sweden, Miami, Scotland, Austria, Switzerland, England and USA.");
+        muralInfo.Add("icon1", "ARTIST - achesdub \nMURAL: The Last King of Ireland");
+        muralInfo.Add("icon2", "ARTIST - Friz \nMURAL: Brigid of Faughart");
+        muralInfo.Add("icon3", "ARTIST - Chula Mente \nMURAL: Ní neart go cur le chéile");
+    }
+
+    private void SetMuralImage(String icon)
+    {
+        if (icon == "icon1")
+        {
+            imagePanel.GetComponent<Image>().sprite = muralImages[0];
+        }
+        else if (icon == "icon2")
+        {
+            imagePanel.GetComponent<Image>().sprite = muralImages[1];
+        }
+        else if (icon == "icon3")
+        {
+            imagePanel.GetComponent<Image>().sprite = muralImages[2];
+        }
     }
 }
